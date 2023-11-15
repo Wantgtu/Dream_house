af3becf71988ddd6e5f8d41ea9e88ecac558e4a6
Juanna <135024930@qq.com>
2023-11-16 01:15:23 +0800

    web

--- web-mobile/src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Main.fire",orientation:"portrait",jsList:["assets/Script/sdk/lib/zs.sdk.js"]};
